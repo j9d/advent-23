4770d61355054ef9b2feefbb370cf04f15300f16
Jamie Dimos <jamiedimos@gmail.com>
2023-12-05 20:35:43 +1100

    Fix day 5

--- 5/first.py
+++ 5/first.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 sequence = [
     "soil",
@@ -9,9 +9,9 @@ sequence = [
     "humidity",
     "location",
 ]
-seeds = []
-destinations = {}
-maps = {
+seeds: List[int] = []
+closest = 999_999_999_999
+maps: Dict[str, List[Dict[str, int]]] = {
     "soil": [],
     "fertilizer": [],
     "water": [],
@@ -29,6 +29,7 @@ def create_map(source: int, dest: int, range: int) -> Dict[str, int]:
     }
 
 def process_seed(seed: int) -> int:
+    # Process seed numbers into destinations
     for mode in sequence:
         for map in maps[mode]:
             diff = map["dest"] - map["source"]
@@ -41,18 +42,23 @@ with open("input.txt", "r") as f:
     mode = "soil"
     for idx, line in enumerate(f):
         if idx == 0:
+            # Import seeds from the first line
             seeds = [int(s) for s in line.split()[1:]]
-            destinations = {s: -1 for s in seeds}
             continue
         data = line.split()
+        # Ignore empty lines
         if not data:
             continue
         if data[1] == "map:":
             # Change mode
             mode = data[0].split("-")[2]
             continue
+        # Import maps from the current mode
         maps[mode].append(create_map(int(data[1]), int(data[0]), int(data[2])))
-for seed in seeds:
-    destinations[seed] = process_seed(seed)
 
-print(min(destinations.values()))
+for seed in seeds:
+    result = process_seed(seed)
+    if result < closest:
+        closest = result
+
+print(closest)
--- 5/second.py
+++ 5/second.py
@@ -10,7 +10,6 @@ sequence = [
     "location",
 ]
 seeds = []
-closest = 999_999_999_999
 maps = {
     "soil": [],
     "fertilizer": [],
@@ -21,7 +20,15 @@ maps = {
     "location": [],
 }
 
+def valid_seed(seed: int) -> bool:
+    for s in seeds:
+        if seed >= s[0] and seed < s[0] + s[1]:
+            return True
+    return False
+
 def chunk_list(arr: List[int]) -> List[List[int]]:
+    # Chunk a list to group adjacent pairs.
+    # e.g. [1, 2, 3, 4, 5, 6] becomes [[1, 2], [3, 4], [5, 6]]
     return [arr[i:i+2] for i in range(0, len(arr), 2)]
 
 def create_map(source: int, dest: int, range: int) -> Dict[str, int]:
@@ -31,11 +38,12 @@ def create_map(source: int, dest: int, range: int) -> Dict[str, int]:
         "range": range,
     }
 
-def process_seed(seed: int) -> int:
-    for mode in sequence:
+def process_reverse(seed: int) -> int:
+    # Process destination numbers back into seeds
+    for mode in sequence[::-1]:
         for map in maps[mode]:
-            diff = map["dest"] - map["source"]
-            if seed >= map["source"] and seed <= map["source"] + map["range"]:
+            diff = map["source"] - map["dest"]
+            if seed >= map["dest"] and seed < map["dest"] + map["range"]:
                 seed += diff
                 break
     return seed
@@ -44,25 +52,28 @@ with open("input.txt", "r") as f:
     mode = "soil"
     for idx, line in enumerate(f):
         if idx == 0:
-            seed_staging = chunk_list([int(s) for s in line.split()[1:]])
-            print(seed_staging)
-            for l in seed_staging:
-                seeds.extend([i for i in range(l[0], l[0] + l[1])])
+            # Import seeds from the first line
+            seeds = chunk_list([int(s) for s in line.split()[1:]])
             continue
         data = line.split()
+        # Ignore empty lines
         if not data:
             continue
         if data[1] == "map:":
             # Change mode
             mode = data[0].split("-")[2]
             continue
+        # Import maps from the current mode
         maps[mode].append(create_map(int(data[1]), int(data[0]), int(data[2])))
-print(len(seeds))
-for idx, seed in enumerate(seeds):
-    if idx % 10_000_000 == 0:
-        print(f'done {idx}')
-    val = process_seed(seed)
-    if val < closest:
-        closest = val
 
-print(closest)
+# Start from the lowest destination number (probably 0).
+# Process destination numbers in reverse to find a seed.
+# If a seed is valid, print it and done.
+destinations = [map["dest"] for map in maps["location"]]
+start = min(destinations)
+stop = max(destinations)
+for seed in range(start, stop):
+    res = process_reverse(seed)
+    if valid_seed(res):
+        print(seed)
+        break
